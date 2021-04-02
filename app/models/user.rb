de39e72fb6cf751ba82ff38a3e77d2ca7c83248e
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-01 21:46:58 -0400

    adds associations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :instruments
+  has_many :categories, through: :instruments
+  has_many :makers, through: :instruments
+  has_many :families, through: :instruments
 end
