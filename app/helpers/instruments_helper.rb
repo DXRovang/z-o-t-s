7a3b49f8573bc745e71b78f82c9cf1371c9573e8
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-02 13:33:55 -0400

    hard codes banjo categories

--- app/helpers/instruments_helper.rb
+++ app/helpers/instruments_helper.rb
@@ -7,11 +7,11 @@ module InstrumentsHelper
     Category.where(id: [1...5])
   end
 
-  def mandolin_categories
-    Category.where(id: [5...8])
-  end
-
   def guitar_categories
     Category.where(id: [8...16])
+  end
+
+  def mandolin_categories
+    Category.where(id: [5...8])
   end
 end
