e7c4dc77f7f8957678fe37a434fd7a0e857e480c
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-01 22:03:42 -0400

    adds omniauth to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,19 @@ class SessionsController < ApplicationController
     end
   end
 
+  def create_with_fb
+    user = User.find_or_create_by(
+      username: self.request.env['omniauth.auth']['info']['name'],
+      email: self.request.env['omniauth.auth']['info']['email']) do |u| 
+        u.password = 'password'
+      end
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      redirect_to signup_path
+    end
+  end
 
   
   def destroy
