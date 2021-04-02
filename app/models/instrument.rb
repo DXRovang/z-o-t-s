90a69d17d56c8c4f6334f199a48dca255ddf02f7
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-01 21:49:19 -0400

    adds simple validations

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -3,4 +3,7 @@ class Instrument < ApplicationRecord
   belongs_to :category
   belongs_to :maker
   belongs_to :family
+
+  validates_presence_of :price, :year, :maker_name
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  include ActiveModel::Validations
+  has_secure_password
+
+  validates_presence_of :username, :email
+  validates_uniqueness_of :username, :email
+  validates_with EmailValidator
+  
   has_many :instruments
   has_many :categories, through: :instruments
   has_many :makers, through: :instruments
