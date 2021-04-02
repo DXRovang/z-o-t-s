c8b83142e247231b61a6a0d6405055eef12e4c4d
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-01 22:00:47 -0400

    fills in user index new create and show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,62 @@
 class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end 
+
+  def create 
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+    if params[:id].to_i != session[:user_id]
+      redirect_to user_path(@user)
+    end
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    if params[:id].to_i != session[:user_id]
+      redirect_to user_path(@user)
+    end
+    @user.instruments.each do |i|
+      i.destroy
+    end
+    @user.destroy
+    redirect_to signup_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :username, 
+      :email, 
+      :password, 
+      :password_confirmation)
+  end
 end
