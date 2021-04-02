e5f911906f10f9cc765009ef5cf4b1674fd78a19
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-02 13:28:38 -0400

    adds methods to instruments_helper

--- app/helpers/instruments_helper.rb
+++ app/helpers/instruments_helper.rb
@@ -1,17 +1,17 @@
 module InstrumentsHelper
-  # def self.family
-  #   self.category.family.name
-  # end
+  def self.family
+    self.category.family.name
+  end
 
-  # def banjo_categories
-  #   Category.where(id: [1...5])
-  # end
+  def banjo_categories
+    Category.where(id: [1...5])
+  end
 
-  # def mandolin_categories
-  #   Category.where(id: [5...8])
-  # end
+  def mandolin_categories
+    Category.where(id: [5...8])
+  end
 
-  # def guitar_categories
-  #   Category.where(id: [8...16])
-  # end
+  def guitar_categories
+    Category.where(id: [8...16])
+  end
 end
