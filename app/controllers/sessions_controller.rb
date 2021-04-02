ef6fd0ea3d6f63088bf1c8553f63e081e371bc51
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-01 22:03:05 -0400

    fills in session new create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,37 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to families_path
+    elsif @user
+      @errors = ["Invalid Password"]
+      render :new
+    else
+      @errors = ["Invalid Username"]
+      render :new
+    end
   end
 
+
+  
   def destroy
+    session.clear
+    redirect_to "/login"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :username, 
+      :email, 
+      :password,
+      :password_confirmation
+    )
   end
 end
