ff51f78e9384bd20e8433dcb1e7cfa2890731c16
Alexander Rovang <wolf_vii@yahoo.com>
2021-04-03 10:58:44 -0400

    refactors instrument create

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,7 +1,87 @@
 class InstrumentsController < ApplicationController
-  def new
+  before_action(:require_login)
+  before_action :set_instrument, only: [:show, :edit, :update, :destroy]
   
-    @instrument = Instrument.new
-    @family = Family.find_by(id: params[:family_id])
+    def index
+      @instruments = Instrument.all
+    end
+  
+    def new
+      @instrument = Instrument.new 
+      @family = Family.find_by(id: params[:family_id])
+    end
+  
+    def create
+      @instrument = Instrument.new(instrument_params)
+      @instrument.user_id = current_user.id
+      # binding.pry
+      if @instrument.save 
+        redirect_to instrument_path(@instrument)
+      else
+        @family = Family.find_by(id: instrument_params[:family_id])
+        render :new
+      end
+    end
+  
+    def show
+     @instrument = Instrument.find_by(id: params[:id])
+    end
+  
+    def edit
+     
+      if @instrument.user_id != current_user.id
+        redirect_to instruments_path
+      else
+        if (1..4).to_a.include?(@instrument.category_id.to_i)
+          @family = Family.find_by(id: 1)
+        elsif (8..15).to_a.include?(@instrument.category_id.to_i)
+          @family = Family.find_by(id: 2)
+        elsif (5..7).to_a.include?(@instrument.category_id.to_i)
+          @family = Family.find_by(id: 3)
+        end
+      end
+    end
+  
+    def update
+    
+      @instrument.update(instrument_params)
+      redirect_to instrument_path(@instrument)
+    end
+  
+    def destroy
+     
+      if @instrument.user_id != current_user.id
+        redirect_to instruments_path
+      else
+        @instrument.destroy
+        redirect_to instruments_path
+      end
+    end
+  
+    private
+  
+    def set_instrument
+      @instrument = Instrument.find_by(id: params[:id])
+    end
+  
+    def instrument_params
+      params.require(:instrument).permit(
+        :price, 
+        :year, 
+        :frets, 
+        :color, 
+        :material, 
+        :hole, 
+        :body, 
+        :neck, 
+        :back, 
+        :bridge, 
+        :description,  
+        :for_sale,
+        :category_id,
+        :family_id,
+        :maker_name,
+      )
+    end
   end
-end
+  
